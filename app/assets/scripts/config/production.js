1c7591cc439caa13821f71bcb8ab84924e132e81
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-10-02 13:47:01 -0400

    chore: Use spaces instead of tabs

--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -4,8 +4,7 @@ var logo = require('./logo');
  * App config for production.
  */
 module.exports = {
-	api: 'https://ocp-server-1f836d591299.herokuapp.com',
-	environment: 'production',
-	consoleMessage: logo
+  api: 'https://ocp-server-1f836d591299.herokuapp.com',
+  environment: 'production',
+  consoleMessage: logo
 };
-
